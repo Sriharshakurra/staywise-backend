ec184336d904701339c2b7507d07f73888576392
Sriharshakurra <64574491+Sriharshakurra@users.noreply.github.com>
2025-06-22 11:33:47 -0500

    Delete admin_site/Shanmukha_project/views.py

--- admin_site/Shanmukha_project/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Service
-
-def services_view(request):
-    services = Service.objects.all()
-    return render(request, 'hostel/services.html', {'services': services})
-
-def home_view(request):
-    return render(request, 'hostel/home.html')
